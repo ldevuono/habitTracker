5076ff152f4925828bdcf2ffcfd9f3cb7f22330b
lauradevuono <lauradevuono@gmail.com>
2023-02-02 16:50:20 -0500

    more styling oops

--- src/Tasks.js
+++ src/Tasks.js
@@ -2,17 +2,19 @@ function Tasks(props) {
 
     return (
         <div className="App">
-            {props.taskList !== [] ?
-                <ul className="taskList">
-                    {props.taskList.map(task =>
-                        <li key={task.id} className={task.isComplete ? "strikethrough" : "listItem"}>
-                            {task.value}
-                            <button className="delete" onClick={(e) => props.deleteTask(e, task.id)}>Delete</button>
-                            <button onClick={(e) => props.completedTask(e, task.id)}>Completed</button>
-                        </li>)}
-                </ul>
-                : null}
-        </div >
+            <div className="taskList">
+                {props.taskList !== [] ?
+                    <ul>
+                        {props.taskList.map(task =>
+                            <li key={task.id} className={task.isComplete ? "strikethrough" : "listItem"}>
+                                {task.value}
+                                <button className="delete" onClick={(e) => props.deleteTask(e, task.id)}>Delete</button>
+                                <button onClick={(e) => props.completedTask(e, task.id)}>Completed</button>
+                            </li>)}
+                    </ul>
+                    : null}
+            </div >
+        </div>
     );
 }
 
